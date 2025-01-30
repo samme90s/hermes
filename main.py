01be2691bbad73e57af8ffb2ccb9ab3d9f8154ff
Sam <samme.s90@gmail.com>
2025-01-30 15:30:43 +0100

    add: spectogram

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 array, sampling_rate = librosa.load(librosa.ex("trumpet"))
 
 plt.figure(figsize=(12, 4))
+# Display the waveform
 librosa.display.waveshow(array, sr=sampling_rate)
 
 # Use this to generate a waveform image
@@ -13,6 +14,8 @@ plt.close()
 
 # #################################
 # Generate a spectrum image
+#
+# Get the first 4096 samples
 dft_input = array[:4096]
 
 # Calculate the DFT (Discrete Fourier Transform)
@@ -35,4 +38,19 @@ plt.xscale("log")
 
 # Use this to generate a spectrum image
 plt.savefig('spectrum.png')
+plt.close()
+
+# #################################
+# Generate a spectrogram image
+
+# Calculate the STFT (Short-Time Fourier Transform)
+D = librosa.stft(array)
+S_db = librosa.amplitude_to_db(np.abs(D), ref=np.max)
+
+plt.figure().set_figwidth(12)
+librosa.display.specshow(S_db, x_axis="time", y_axis="hz")
+plt.colorbar()
+
+# Use this to generate a spectrogram image
+plt.savefig('spectrogram.png')
 plt.close()
