a4895d6284d8ff9e0b36f7f8b4484efd3b724b2f
Sam <samme.s90@gmail.com>
2025-02-13 09:27:09 +0100

    move files

--- pyasr/main.py
+++ pyasr/main.py
@@ -4,12 +4,12 @@ from concurrent import futures
 from grpc import ServicerContext
 from grpc import server as grpc_server
 
-from pyasr_pb2 import transcription_reply, transcription_request
-from pyasr_pb2_grpc import ProtoServicer, add_ProtoServicer_to_server
+from main_pb2 import transcription_reply, transcription_request
+from main_pb2_grpc import MainServicer, add_MainServicer_to_server
 from src.whisper import transcribe_audio_base64
 
 
-class Server(ProtoServicer):
+class Servicer(MainServicer):
     def transcribe_audio(self, request: transcription_request, context: ServicerContext) -> transcription_reply:
         transcription = transcribe_audio_base64(
             audio_base64=request.audio_base64,
@@ -21,7 +21,7 @@ class Server(ProtoServicer):
 def serve():
     port = "50051"
     server = grpc_server(futures.ThreadPoolExecutor(max_workers=10))
-    add_ProtoServicer_to_server(Server(), server)
+    add_MainServicer_to_server(Servicer(), server)
     server.add_insecure_port("[::]:" + port)
     server.start()
     print("Server started, listening on " + port)
