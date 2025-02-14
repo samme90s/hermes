b0ea02e776513a3e3437bde65d12f3b0a37f96ec
Sam <samme.s90@gmail.com>
2025-02-14 17:17:51 +0100

    update info to log

--- pystt/main.py
+++ pystt/main.py
@@ -22,15 +22,15 @@ def _process_error(exc) -> JSONResponse:
     if isinstance(exc, ValueError):
         # Perhaps return a static string here?
         return JSONResponse(
-                status_code=400,
-                content={"error": str(exc)}
-                )
+            status_code=400,
+            content={"error": str(exc)}
+        )
 
     # Default response for any other error
     return JSONResponse(
-            status_code=500,
-            content={"error": "Internal server error"}
-            )
+        status_code=500,
+        content={"error": "Internal server error"}
+    )
 
 
 try:
@@ -48,7 +48,7 @@ try:
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
-        )
+    )
 
     @app.middleware("http")
     async def log_requests(request: Request, call_next):
@@ -56,7 +56,12 @@ try:
         Middleware to log requests and responses using "HTTP" log level
         '''
         start_time = time.time()
-    
+      
+        # Log request info
+        client_ip = request.client.host if request.client else "missing"
+        logger.info(f"ip: {client_ip}")
+        logger.info(f"m: {request.method} {request.url.path}")
+
         try:
             # Process the request
             response = await call_next(request)
@@ -67,8 +72,7 @@ try:
         process_time = (time.time() - start_time) * 1000
         formatted_process_time = f"{process_time:.2f}ms"
 
-        # Log the request completion along with method, path, status, and processing time.
-        logger.info(f"m: {request.method} {request.url.path}")
+        # Log response info and process time
         logger.info(f"s: {response.status_code}")
         logger.info(f"t: {formatted_process_time}")
 
@@ -76,16 +80,16 @@ try:
 
     @app.post("/transcribe")
     async def post_transcribe(audio_file: UploadFile = File(...)):
+        # Log file info for debug purposes
         logger.debug(f"Recieved files: {audio_file.filename}")
         logger.debug(f"Content-Type: {audio_file.content_type}")
-
         audio_bytes = await audio_file.read()
-        logger.debug(f"File size: {len(audio_bytes)} bytes"
-                     )
+        logger.debug(f"File size: {len(audio_bytes)} bytes")
+
         transcription = transcribe(
             audio_bytes=audio_bytes,
-            language="sv"
-            )
+            language="en"
+        )
         return {"transcription": transcription}
 
 except Exception as exc:
