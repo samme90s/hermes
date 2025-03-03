704c40bf6527d6d8e00a8fecfe970ae6f116735d
Sam <samme.s90@gmail.com>
2025-03-03 12:45:15 +0100

    remove electron

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-    // This is important for the file paths to be correct when running
-    // Electron!
-    base: "./",
     plugins: [react(), tailwindcss()],
-});
+})
