827eaba7088ea1cb6f2d02b5971084eb02db6a37
Sam <samme.s90@gmail.com>
2025-03-03 11:23:53 +0100

    add electron support

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,5 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+    // This is important for the file paths to be correct when running
+    // Electron!
+    base: "./",
     plugins: [react(), tailwindcss()],
 });
